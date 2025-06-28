d9873b485143a291c8db138e767bb61cc7179f2f
Kirill Biller <biller.k@yandex.ru>
2025-06-29 02:00:21 +0300

    Added gridjs

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
+import { Grid } from "gridjs"
+import { ruRU } from "gridjs/l10n"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    new Grid({
+      search: true,
+      sort: true,
+      resizable: true,
+      fixedHeader: true,
+      language: ruRU,
+      columns: ["Name", "Email", "Phone Number"],
+      data: [
+        ["John", "john@example.com", "(353) 01 222 3333"],
+        ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
+        ["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
+        ["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
+        ["Afshin", "afshin@mail.com", "(353) 22 87 8356"]
+      ],
+    }).render(this.element)
   }
 }
