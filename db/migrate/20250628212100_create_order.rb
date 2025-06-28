0f81e1ed860fad3e187f129510b174ffb2b87f7c
Kirill Biller <biller.k@yandex.ru>
2025-06-29 00:53:50 +0300

    Add due_date column to orders table

--- db/migrate/20250628212100_create_order.rb
+++ db/migrate/20250628212100_create_order.rb
@@ -1,6 +1,7 @@
 class CreateOrder < ActiveRecord::Migration[7.1]
   def change
     create_table :orders do |t|
+      t.datetime :due_date, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_28_212113) do
   end
 
   create_table "orders", force: :cascade do |t|
+    t.datetime "due_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
